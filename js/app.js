315e6638c4e9f1b559bd9882c271c276056b7ec9
Oscar Calderon <ocalderon@medline.com>
2025-10-05 18:10:34 -0500

    week 4 final

--- js/app.js
+++ js/app.js
@@ -4,12 +4,13 @@
 //Arreglos
 const ingresos = [
   new Ingreso('Salario', 20000),
-  new Ingreso('Venta auto', 50000)
+  //new Ingreso('Venta auto', 50000),
 ];
 
 const egresos = [
   new Egreso('Renta', 4000),
-  new Egreso('Ropa', 800)
+  //new Egreso('Ropa', 800),
+  
 ];
 
 // Función flecha para formatear valores como porcentaje
@@ -52,10 +53,114 @@ const cargarCabecero = () => {
   const presupuesto = totalIngresos() - totalEgresos();
   const porcentajeEgreso = totalEgresos() / totalIngresos();
 
-  console.log(formatoMoneda(presupuesto));
-  console.log(formatoPorcentaje(porcentajeEgreso));
-  console.log(formatoMoneda(totalIngresos()));
-  console.log(formatoMoneda(totalEgresos()));
+document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
+
+  if (formatoPorcentaje(porcentajeEgreso) !== 'NaN%' && formatoPorcentaje(porcentajeEgreso) !== '∞%') {
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
+  } else {
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(0);
+  }
+  
+  //document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
+  document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+  document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
+
+  // console.log(formatoMoneda(presupuesto));
+  // console.log(formatoPorcentaje(porcentajeEgreso));
+  // console.log(formatoMoneda(totalIngresos()));
+  // console.log(formatoMoneda(totalEgresos()));
 };
 
-cargarCabecero();
+function cargarApp() {
+  cargarCabecero();
+  cargarIngresos();
+  cargarEgresos();
+};
+
+const cargarIngresos = () => {
+  let ingresosHTML = '';
+  for (const ingreso of ingresos) {
+    ingresosHTML += crearIngresoHTML(ingreso);
+  }
+  document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
+};
+
+const crearIngresoHTML = ingreso => {
+  let ingresoHTML = `<div class="elemento limpiarEstilos">`;
+  ingresoHTML += `<div class="elemento_descripcion">${ingreso.descripcion}</div>`;
+  ingresoHTML += `<div class="derecha limpiarEstilos">`;
+  ingresoHTML += `<div class="elemento_valor">${formatoMoneda(ingreso.valor)}</div>`;
+  ingresoHTML += `<div class="elemento_eliminar">`;
+  ingresoHTML += `<button class="elemento_eliminar--btn">`;
+  ingresoHTML += `<ion-icon name="close-circle-outline" onclick="eliminarIngreso(${ingreso.id})"></ion-icon>`;
+  ingresoHTML += `</button>`;
+  ingresoHTML += `</div>`;
+  ingresoHTML += `</div>`;
+  ingresoHTML += `</div>`;
+  return ingresoHTML;
+};
+
+const cargarEgresos = () => {
+  let egresosHTML = '';
+  for (const egreso of egresos) {
+    egresosHTML += crearEgresoHTML(egreso);
+  }
+  document.getElementById('lista-egresos').innerHTML = egresosHTML;
+};
+
+const crearEgresoHTML = egreso => {
+  let egresoHTML = `<div class="elemento limpiarEstilos">`;
+  egresoHTML += `<div class="elemento_descripcion">${egreso.descripcion}</div>`;
+  egresoHTML += `<div class="derecha limpiarEstilos">`;
+  egresoHTML += `<div class="elemento_valor">${formatoMoneda(egreso.valor)}</div>`;
+  egresoHTML += `<div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor / totalEgresos())}</div>`;
+  egresoHTML += `<div class="elemento_eliminar">`;
+  egresoHTML += `<button class="elemento_eliminar--btn">`;
+  egresoHTML += `<ion-icon name="close-circle-outline" onclick="eliminarEgreso(${egreso.id})"></ion-icon>`;
+  egresoHTML += `</button>`;
+  egresoHTML += `</div>`;
+  egresoHTML += `</div>`;
+  egresoHTML += `</div>`;
+  return egresoHTML;
+};
+
+const eliminarEgreso = id => {
+  const indiceEliminar = egresos.findIndex(egreso => egreso.id === id);
+  egresos.splice(indiceEliminar, 1);
+  cargarCabecero();
+  cargarEgresos();
+};
+
+const eliminarIngreso = id => {
+  const indiceEliminar = ingresos.findIndex(ingreso => ingreso.id === id);
+  ingresos.splice(indiceEliminar, 1);
+  cargarCabecero();
+  cargarIngresos();
+};
+
+const agregarDato = () => {
+  const forma = document.forms['forma'];
+  const tipo = forma['tipo'];
+  const descripcion = forma['descripcion'];
+  const valor = forma['valor'];
+  
+  if (descripcion.value !== '' && valor.value !== '') {
+    if (tipo.value === 'ingreso') {
+      ingresos.push(new Ingreso(descripcion.value, +valor.value));
+      cargarIngresos();
+      cargarCabecero();
+    } else if (tipo.value === 'egreso') {
+      egresos.push(new Egreso(descripcion.value, +valor.value));
+      cargarEgresos();
+      cargarCabecero();
+    }
+  }
+  descripcion.value = '';
+  valor.value = '';
+};
+
+window.addEventListener('load', cargarApp);
+document.getElementById('agregarValor_btn').addEventListener('click', agregarDato);
+
+window.eliminarEgreso = eliminarEgreso;
+window.eliminarIngreso = eliminarIngreso;
